918c22c97c7c6d769bdccb38e1ccac6cc7740766
Hrishitcodethis <hrishityelchuri2017@gmail.com>
2025-09-27 14:29:13 +0530

    notebook experimentation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Hrishit Yelchuri',
+    author_email= 'hrishityelchuri2017@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
